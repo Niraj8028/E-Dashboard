0732f55cfdcf58dd61efdd2ee94525e3f4c7658b
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-06 17:29:27 +0530

    mongoose connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,16 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const app=express();
 
-
+const connectDB= async()=>{
+    mongoose.connect('mongodb://localhost:27017/T-shirts')
+    const productSchema=new mongoose.Schema({});
+    const products=mongoose.model('products',productSchema);
+    const data=await products.find();
+    console.log(data);
+}
+ 
+connectDB();
 app.get('/',(req,res)=>{
     res.send("Connected")
 })
