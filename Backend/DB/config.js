a12c2b8bab6c10c51029ef69d4924f2e57432386
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-07 20:02:17 +0530

    routes created successfully

--- Backend/DB/config.js
+++ Backend/DB/config.js
@@ -1,3 +1,3 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/T-shirts;')
+mongoose.connect('mongodb://localhost:27017/test')
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,18 @@
 const express=require('express')
 require('./DB/config')
+const User=require('./DB/User')
+
 const app=express();
+app.use(express.json());
 
-
-
-
+app.post('/register',async(req,res)=>{
+    let user= new User(req.body);
+    let result=await user.save();
+    res.send(result)
+})
+app.get('/',(req,res)=>{    
+    res.send("Success")
+})
 
 app.listen(8000,(req,res)=>{
     console.log("Connected");
