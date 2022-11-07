3c443465c77a7096a121f1ed8b9c1774bf4ac01e
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-07 19:36:57 +0530

    userschema defined

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,11 @@
 const express=require('express')
-const mongoose=require('mongoose')
+require('./DB/config')
 const app=express();
 
-const connectDB= async()=>{
-    mongoose.connect('mongodb://localhost:27017/T-shirts')
-    const productSchema=new mongoose.Schema({});
-    const products=mongoose.model('products',productSchema);
-    const data=await products.find();
-    console.log(data);
-}
- 
-connectDB();
-app.get('/',(req,res)=>{
-    res.send("Connected")
-})
+
+
+
+
 app.listen(8000,(req,res)=>{
     console.log("Connected");
 })
