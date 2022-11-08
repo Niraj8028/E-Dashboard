1f9406db27637d4b31ce501b062b777f754285ae
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-11-08 16:21:59 +0530

    login and register done

--- Backend/index.js
+++ Backend/index.js
@@ -10,11 +10,25 @@ app.use(cors())
 app.post('/register',async(req,res)=>{
     let user= new User(req.body);
     let result=await user.save();
+    result=result.toObject();
+    delete result.password;
     res.send(result)
 })
 app.post('/login',async(req,res)=>{
-    let user=await User.findOne(req.body);
-    res.send(user);
+    let user=await User.findOne(req.body).select("-password");
+    if(req.body.email && req.body.password){
+        if(user){
+            res.send(user);
+        }
+        else{
+            res.send({Result:"No user Found"})
+        }
+    }
+    else{
+        res.send({Result:"No user Found"})
+    }
+    
+    
 })
 app.get('/',(req,res)=>{    
     res.send("Success")
